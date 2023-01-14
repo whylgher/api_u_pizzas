e534cbeaa99312244b4133ba141e4526aff57735
whyllima <7bitsmarket@gmail.com>
2023-01-14 16:49:24 +0000

    IMP orders MMC

---
+++ database/migrations/2023_01_13_160258_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->enum('status', ['WAITING', 'PROGRESS', 'COMPLETED', 'CANCELED'])->nullable(false);
+            $table->double('amount');
+            $table->double('tax');
+            $table->double('total');
+            $table->json('order');
+            $table->json('drink');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
