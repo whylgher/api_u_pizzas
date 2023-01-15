60782de941891d252c0a2639e2fc9f1ccd1a37c3
whyllima <7bitsmarket@gmail.com>
2023-01-15 20:48:50 +0000

    add functions

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -12,29 +12,39 @@ class OrderController extends Controller
         $this->middleware('auth:api');
     }
 
+    public function index()
+    {
+        $order = Order::all();
+        return response()->json([
+            "message" => "success",
+            "order" => $order
+        ]);
+    }
+
+    public function show($id)
+    {
+        return Order::find($id);
+    }
+
+    public function showIdUser($id)
+    {
+        $order = Order::all()->where('user_id', '=', $id);
+        return response()->json([
+            "message" => "success",
+            "order" => $order
+        ]);
+    }
+
     public function store(Request $request)
     {
-        // $order = new Order();
-
-        // $order->name = $request->name;
-        // $order->user_id = $request->user_id;
-        // $order->description = $request->description;
-        // $order->status = $request->status;
-        // $order->amount = $request->amount;
-        // $order->tax = $request->tax;
-        // $order->total = $request->total;
-        // $order->order = $request->order;
-        // $order->drink = $request->drink;
-
-        // dd($order);
-
-        // $data = $order->save();
-
-        // return response()->json([
-        //     "message" => "success",
-        //     "data" => $data
-        // ], 200);
-
-        return Order::create($request->all());
+        $order = Order::create($request->all());
+        if ($order) {
+            return response()->json([
+                "message" => "success",
+                "order_id" => $order->id,
+                "order" => $order,
+                "status" => 200
+            ]);
+        }
     }
 }
